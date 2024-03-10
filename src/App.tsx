26feb664ef848192140a5b86425cf7ff925e2c5d
ralvarezigarzabal <ralvarezigarzabal@developmentgateway.org>
2024-03-10 20:03:39 -0300

    delete css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import csstesting from './csstesting.css'
 import firebase from './Config/firebase.js'
 
 
